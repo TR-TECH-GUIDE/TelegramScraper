d77a8fb40b1a03184c981c33f52cc2d6e27d85d1
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-08 18:49:33 +0530

    Update add2group.py

--- add2group.py
+++ add2group.py
@@ -26,7 +26,7 @@ def banner():
 {re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
 
             Version: 1.3
-     Modified by @AbirHasan2005
+     Modified by @TR-TECH-GUIDE
         """)
 
 cpass = configparser.RawConfigParser()
