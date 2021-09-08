20beac9a1c0827dd37f5ae8a7e4fea7782c15547
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-08 18:46:44 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -1,6 +1,5 @@
 #!/bin/env python3
-# (c) @AbirHasan2005
-# Telegram Group: http://t.me/linux_repo
+# (c) @TR-TECH-GUIDE
 # Please give me credits if you use any codes from here.
 
 
@@ -9,7 +8,7 @@ print ("░█▀█░█▀▄░█▀▄░█▀▀░█▀▄")
 print ("░█▀█░█░█░█░█░█▀▀░█▀▄")
 print ("░▀░▀░▀▀░░▀▀░░▀▀▀░▀░▀")
 print ("")
-print ("      by \033[1;95m@AbirHasan2005")
+print ("      by \033[1;95m@TR-TECH-GUIDE")
 print ("\033[1;92m")
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
